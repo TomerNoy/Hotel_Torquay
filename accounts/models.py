ee5adaefeeb6530fbd531cc6ea6804428ea91ff4
Lsh4d <leonardo.mengana@gmail.com>
2021-12-02 17:11:48 +0200

    created branch backend_merged with frontend 2th commit_added fake model.py

---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=100)
+    # profession = models.CharField(max_length=100)
+    # address = models.CharField(max_length=100, null=True)
+    # github = models.CharField(max_length=100, null=True)
+    # facebook = models.CharField(max_length=100, null=True)
+    # twitter = models.CharField(max_length=100, null=True)
+    # instagram = models.CharField(max_length=100, null=True)
+    # website = models.CharField(max_length=100, null=True)
